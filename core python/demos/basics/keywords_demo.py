cc5fd4137f02056d72893afe6b1715c787c07d07
ayush_renuse_FBS_work <renuseayush838@gmail.com>
2026-07-08 15:32:00 +0530

    Add files via upload

---
+++ core python/demos/basics/keywords_demo.py
@@ -0,0 +1,6 @@
+#1.predefined words - special meaning in python
+#2.reserved words - can't use as an identifier
+
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
---
+++ core python/demos/basics/operators/logicalOP.py
@@ -0,0 +1,8 @@
+#1. and : if both conditions are true then return true otherwise false.
+print(True and True)
+
+#2. or : if both conditions are false then return false otherwise true. 
+print(False or False)
+
+#3. not : opposite of condition 
+print(not True)
